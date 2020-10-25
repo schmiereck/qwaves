99be84315d279ab5afd3344007c4171965d61d4d
smko4b <schmiereck@open4business.de>
2020-10-25 16:29:55 +0100

    Implement Probability.

--- src/test/java/de/schmiereck/qwaves/ProbabilityCalcTest.java
+++ src/test/java/de/schmiereck/qwaves/ProbabilityCalcTest.java
@@ -54,8 +54,19 @@ public class ProbabilityCalcTest {
     @org.junit.jupiter.api.Test
     void calc40() {
         // arrange
-        //                        012345678901234567890123
-        final String expected1 = "1..1..1..1..1.1.1.1.1.1.";
+        // 40%:	4,8=40%*12		8,333=100/12 	33,333=8,333*4	41,666=8,333*5%	6,666=2*3,333	6,666=4*1,666
+        // 0: 480%=40%*12		4=480%/100%	3=12/4	400%=4*100%	80%=480%-400%
+        // 1: 560%=40%*12+80%	5=560%/100%	2=12/5	500%=5*100%	60%=480%-500%+80
+        // 2: 540%=40%*12+60%	5=540%/100%	2=12/5	500%=5*100%	40%=480%-500%+60%
+        // 3: 520%=40%*12+40%	5=520%/100%	2=12/5	500%=5*100%	20%=480%-500%+40%
+        // 4: 500%=40%*12+20%	5=500%/100%	2=12/5	500%=5*100%	0%=480%-500%+20%
+        // 5: 480%=40%*12+0%	4=480%/100%	3=12/4	400%=4*100%	80%=480%-400%
+        final String expected1 =
+        //       012345678901     234567890123456
+        //       012345678901     012345678901     012345678901     012345678901
+                "1..1..1..1.." + "1.1.1.1.1.1." + "1.1.1.1.1.1." + "1.1.1.1.1.1." + "1.1.1.1.1.1." + "1..1..1..1.." +
+                "1.1.1.1.1.1." + "1.1.1.1.1.1." + "1.1.1.1.1.1." + "1.1.1.1.1.1." + "1..1..1..1.." + "1.1.1.1.1.1." +
+                "1.1.1.1.1.1." + "1.1.1.1.1.1." + "1.1.1.1.1.1." + "1.1.1.1.1.1." + "1.1.1.1.1.1." + "1.1.1.1.1.1.";
         final ProbabilityCalc probabilityCalc = new ProbabilityCalc(40);
 
         // assert
@@ -66,7 +77,7 @@ public class ProbabilityCalcTest {
         final AtomicInteger pos = new AtomicInteger();
         expected1.chars().forEach(c -> {
             final boolean b = probabilityCalc.calcTick();
-            if (c == '1') assertTrue(b, String.format("On position \"%s\".", pos)); else assertFalse(b, String.format("On position \"%s\".", pos));
+            if (c == '1') assertTrue(b, String.format("On position \"%s\" (rangePos:%d, posInRange:%d).", pos, probabilityCalc.rangePos, probabilityCalc.posInRange)); else assertFalse(b, String.format("On position \"%s\" (rangePos:%d, posInRange:%d).", pos, probabilityCalc.rangePos, probabilityCalc.posInRange));
             pos.getAndIncrement();
         });
     }
